61232a2dfc00052070d5f51ee3db869efec89f11
Aleksey Chernov <alexidal@mail.ru>
2024-09-04 19:11:36 +0300

    added Discovery

--- src/main/java/ru/skillbox/SocialNetworkDialogApplication.java
+++ src/main/java/ru/skillbox/SocialNetworkDialogApplication.java
@@ -2,11 +2,12 @@ package ru.skillbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Page;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class SocialNetworkDialogApplication {
 
 	public static void main(String[] args) {
