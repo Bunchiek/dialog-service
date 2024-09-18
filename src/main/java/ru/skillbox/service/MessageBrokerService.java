7374fd535b3dc7ea4361a6dffd50b48846c18a2a
Aleksey Chernov <alexidal@mail.ru>
2024-09-18 18:35:05 +0300

    deleted comments

--- src/main/java/ru/skillbox/service/MessageBrokerService.java
+++ src/main/java/ru/skillbox/service/MessageBrokerService.java
@@ -3,7 +3,6 @@ package ru.skillbox.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import ru.skillbox.configuration.RabbitMQConfig;
 import ru.skillbox.dto.MessageDto;
@@ -13,11 +12,13 @@ import ru.skillbox.dto.MessageDto;
 @Slf4j
 public class MessageBrokerService {
 
-    private final SimpMessagingTemplate messagingTemplate;
+//    private final SimpMessagingTemplate messagingTemplate;
+    private final MessageConsumerService messageConsumerService;
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void receiveMessage(MessageDto messageDTO) {
         // Обработка сообщения и отправка его обратно клиентам через WebSocket
-        messagingTemplate.convertAndSend("/topic/public", messageDTO);
+//        messagingTemplate.convertAndSend("/topic/public", messageDTO);
+        messageConsumerService.saveMessage(messageDTO);
     }
 }
