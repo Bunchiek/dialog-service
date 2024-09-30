b8815c58a3e832d86c963644f9bfc8ab775276aa
Aleksey Chernov <alexidal@mail.ru>
2024-09-30 17:00:08 +0300

    changed id

--- src/main/java/ru/skillbox/service/MessageBrokerService.java
+++ src/main/java/ru/skillbox/service/MessageBrokerService.java
@@ -1,5 +1,6 @@
 package ru.skillbox.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Component;
 import ru.skillbox.configuration.RabbitMQConfig;
 import ru.skillbox.dto.MessageWebSocketDto;
 import ru.skillbox.dto.MessageWebSocketRs;
+import ru.skillbox.entity.Dialog;
+import ru.skillbox.repository.DialogRepository;
 
 @Component
 @RequiredArgsConstructor
@@ -16,12 +19,16 @@ public class MessageBrokerService {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final MessageConsumerService messageConsumerService;
+    private final DialogRepository dialogRepository;
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void receiveMessage(MessageWebSocketDto messageWebSocketDTO) {
         // Отправляем сообщение обратно клиентам через WebSocket
+        Dialog currentDialog = dialogRepository
+                .findByParticipants(messageWebSocketDTO.getData().getConversationPartner1(),messageWebSocketDTO.getData().getConversationPartner2())
+                .orElseThrow(() -> new EntityNotFoundException("Диалог не найден"));
         MessageWebSocketRs rs = messageWebSocketDTO.getData();
-        rs.setId(1L);
+        rs.setId(currentDialog.getId());
         messageWebSocketDTO.setData(rs);
         messagingTemplate.convertAndSend("/topic/public", messageWebSocketDTO);
         messageConsumerService.saveMessage(messageWebSocketDTO);
