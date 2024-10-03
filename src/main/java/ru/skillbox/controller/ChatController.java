0a18345406b54a8f93c2d4166609134680735a5d
Aleksey Chernov <alexidal@mail.ru>
2024-10-03 13:53:50 +0300

    changed broker to internal

--- src/main/java/ru/skillbox/controller/ChatController.java
+++ src/main/java/ru/skillbox/controller/ChatController.java
@@ -4,23 +4,31 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import ru.skillbox.annotation.Loggable;
 import ru.skillbox.configuration.RabbitMQConfig;
 import ru.skillbox.dto.MessageWebSocketDto;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class ChatController {
 
-    private final RabbitTemplate rabbitTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @Loggable
     @MessageMapping("/chat.sendMessage")
     public void sendMessage(MessageWebSocketDto messageWebSocketDTO) {
-        String routingKey = "topic.dialog." + messageWebSocketDTO.getData().getId();
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, routingKey, messageWebSocketDTO);
-        log.info("Message sent to RabbitMQ with routing key: " + routingKey);
+
+
+//        String routingKey = "topic.dialog." + messageWebSocketDTO.getData().getId();
+//        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, routingKey, messageWebSocketDTO);
+//        log.info("Message sent to RabbitMQ with routing key: " + routingKey);
+
+        String destination = "/topic/dialog/" + messageWebSocketDTO.getData().getId();
+        messagingTemplate.convertAndSend(destination, messageWebSocketDTO);
+        log.info("Message sent to WebSocket topic: " + destination);
     }
 }
