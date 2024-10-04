017a06cb46e8106f133245ca5abbf37bda88c821
Aleksey Chernov <alexidal@mail.ru>
2024-10-04 11:15:34 +0300

    added WS logging

--- src/main/java/ru/skillbox/configuration/WebSocketConfig.java
+++ src/main/java/ru/skillbox/configuration/WebSocketConfig.java
@@ -3,14 +3,18 @@ package ru.skillbox.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.*;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer{
+public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
 
     @Value("${app.rabbitMQ.host}")
     private String rabbitMqHost;
@@ -40,6 +44,16 @@ public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer{
                 .setAllowedOriginPatterns("*");
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                System.out.println("Received message from client: " + message);
+                return message;
+            }
+        });
+    }
 }
 
 
